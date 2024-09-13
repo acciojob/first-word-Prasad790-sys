a9603c4fc477a6ec03ddbb647ed30d74117d4ab1
Prasad790-sys <psa19974@gmail.com>
2024-09-13 22:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	let ct=0;
+	for(c of s){
+		ct++;
+		if(c===''& ct===0) return '';
+		else if(c===' ') return s.slice(0,ct);
+	}
+	return s;
 }
 
-// Do not change the code below
-
+// Do not change the code be
 const s = prompt("Enter String:");
 alert(firstWord(s));
